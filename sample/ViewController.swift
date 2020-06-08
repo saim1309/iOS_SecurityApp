772ed3f8e7cfb233eea10908cf8a5062231e0167
Saim <saimahmad1234@gmail.com>
2020-06-08 01:34:31 -0400

    Basic UI for login and Register w/o constraints

---
+++ sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by user174353 on 5/28/20.
+//  Copyright © 2020 user174353. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
